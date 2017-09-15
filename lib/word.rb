a657769b13e6ef60fb2dc7aa622b22962271bd3d
Lainey <laineyNrokk@gmail.com>
2017-09-15 15:44:24 -0700

    add the instances method of #save

--- lib/word.rb
+++ lib/word.rb
@@ -1,14 +1,20 @@
 class Word
+  attr_reader :
   attr_accesor(:word, :definition)
   @@word_list = []
 
   def initialize(word, definition)
     @word = word
     @definition = []
+    @id = @@word_list.length + 1
   end
 
   def self.all()
     @@word_list
+  end
+
+  def save()
+    @@word_list.push(self)
   end
 
 
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -6,3 +6,11 @@ describe(".all") do
     expect(Word.all()).to(eq([]))
   end
 end
+
+describe("#save") do
+  it("saves a word to the word list") do
+    word = Word.new("arcanum")
+    word.save()
+    expect(Word.all()).to(eq([word]))
+  end
+end
